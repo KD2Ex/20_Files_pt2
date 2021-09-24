e9d22a0f941bb4ed4e810ed825390dae83ba3261
Whiplash <kountdown@mail.ru>
2021-09-24 18:48:33 +0300

    All tasks completed

--- Task4_ATM.cpp
+++ Task4_ATM.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 using namespace std;
 
+const int BILLS[] = {100, 200, 500, 1000, 2000, 5000};
+
 int atm_statement(ifstream &file, string path = "atm.txt") {
     if (!file.is_open()) {
         file.open(path, ios::binary);
@@ -16,17 +18,82 @@ int atm_statement(ifstream &file, string path = "atm.txt") {
     return billsCount;
 }
 
+void countAllBills(int bills[], int * billsCount) {
+    //billsCount[6] = {0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < 1000; ++i) {
+        switch (bills[i]) {
+            case 5000:
+                billsCount[5]++;
+                break;
+            case 2000:
+                billsCount[4]++;
+                break;
+            case 1000:
+                billsCount[3]++;
+                break;
+            case 500:
+                billsCount[2]++;
+                break;
+            case 200:
+                billsCount[1]++;
+                break;
+            case 100:
+                billsCount[0]++;
+                break;
+        }
+    }
+}
+
+void billWithdrawal (int * bills, int count, int denomination) {
+    for (int i = 0; i < count; ++i) {
+        int j = 0;
+        while (bills[j] != denomination) {
+            ++j;
+        }
+        bills[j] = 0;
+    }
+}
+
+bool isWithdrawalPossible(int sum, int * bills) {
+    int billsCount[6];
+    billsCount[5] = sum / 5000;
+    sum %= 5000;
+    billsCount[4] = sum / 2000;
+    sum %= 2000;
+    billsCount[3] = sum / 1000;
+    sum %= 1000;
+    billsCount[2] = sum / 500;
+    sum %= 500;
+    billsCount[1] = sum / 200;
+    sum %= 200;
+    billsCount[0] = sum / 100;
+    sum %= 100;
+
+    int billsInAtm[] =  {0, 0, 0, 0, 0, 0};
+    countAllBills(bills, billsInAtm);
+
+    if (sum == 0) {
+        if (billsCount[5] <= billsInAtm[5] && billsCount[4] <= billsInAtm[4] && billsCount[3] <= billsInAtm[3]
+            && billsCount[2] <= billsInAtm[2] && billsCount[1] <= billsInAtm[1] && billsCount[0] <= billsInAtm[0]) {
+
+            for (int i = 0; i < 6; ++i) {
+                billWithdrawal(bills, billsCount[i], BILLS[i]);
+            }
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
 int main() {
     ofstream atm_out("atm.txt", ios::app);
     ifstream atm_in("atm.txt", ios::binary);
-    srand(time(nullptr));
-    ifstream file;
-    const int BILLS[] = {100, 200, 500, 1000, 2000, 5000};
     char operation;
-    //cout << atm_statement(file, "atm.txt") << " of bills\n";
     cin >> operation;
 
     if (operation == '+') {
+        srand(time(nullptr));
         string str;
         for (int i = 0; i < 1000; ++i) {
             atm_in >> str;
@@ -35,10 +102,30 @@ int main() {
             }
         }
     } else if (operation == '-') {
+        int billsInAtm[1000];
+
+        for (int i = 0; !atm_in.eof() ; ++i) {
+            atm_in >> billsInAtm[i];
+        }
+
         int withdrawal;
         cout << "Enter a withdrawal:" << endl;
         cin >> withdrawal;
 
+        if (isWithdrawalPossible(withdrawal, billsInAtm)) {
+            cout << "You withdrawn a " << withdrawal << " amount of money\n";
+            atm_out.close();
+
+            atm_out.open("atm.txt");
+            for (int i = 0; i < 1000; ++i) {
+                if (billsInAtm[i] == 0 || billsInAtm[i] > 5000) {
+                    continue;
+                }
+                atm_out << billsInAtm[i] << "\n";
+            }
+        } else {
+            cout << "ATM cannot process this operation\n";
+        }
     }
     atm_out.close();
     atm_in.close();
